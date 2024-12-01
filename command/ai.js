e66423eea6cde44b13c43b0eab273713922a2db5
ARYAN <itszaryan593@gmail.com>
2024-12-01 17:38:00 +0600

    Create ai.js

---
+++ command/ai.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+
+module.exports = {
+  name: 'ai',
+  description: 'Ask a question to chatgpt',
+  author: 'ArYAN',
+  async execute(senderId, args, pageAccessToken, sendMessage) {
+    const prompt = args.join(' ');
+    if (prompt === "") {
+      sendMessage(senderId, { text: "Usage: /ai <question>" }, pageAccessToken);
+      return; // Ensure the function doesn't continue
+    }
+    // Inform the user that content is being generated
+    sendMessage(senderId, { text: 'Generating content... Please wait.' }, pageAccessToken);
+    try {
+      const apiUrl = 'http://markdevs-last-api.onrender.com/api/v2/gpt4';
+      const response = await axios.post(apiUrl, { query: prompt });
+      const text = response.data.gpt4;
+      // Send the generated text to the user
+      sendMessage(senderId, { text: "GPT4 BY CHATGPT:\n\n" + text }, pageAccessToken);
+    } catch (error) {
+      console.error('Error calling GPT-4 API:', error);
+      sendMessage(senderId, { text: 'There was an error generating the content. Please try again later.' }, pageAccessToken);
+    }
+  }
+};
+    
